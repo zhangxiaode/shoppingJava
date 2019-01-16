15dbb38be7b20b9bae9eaaefce7567fbf7062fe9
zhangxiaode <2279323443@qq.com>
2019-01-16 17:30:09 +0800

    接口response格式统一处理

--- src/main/java/com/zxd/shopping/controller/LoginController.java
+++ src/main/java/com/zxd/shopping/controller/LoginController.java
@@ -1,22 +1,21 @@
 package com.zxd.shopping.controller;
 
-//import com.zxd.shopping.bean.Login;
-//import org.springframework.beans.factory.annotation.Autowired;
+import com.zxd.shopping.bean.Login;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpMethod;
 
-
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-//import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.TypeReference;
 
+import com.zxd.shopping.utils.ResultUtil;
 
 @RestController
 @RequestMapping("/apis")
@@ -31,32 +30,31 @@ public class LoginController {
     @Value("${wechat.grantType}")
     private String grantType;
 
-//    @Autowired
-//    private Login login;
-
-//    @RequestMapping(value="/login2", method = RequestMethod.POST)
-//    public String login2(@RequestBody String code){
-//        System.out.println(code);
-//        String jsontext="{\"name\":\"wjk\",\"age\":\"22\",\"love\":[{\"love1\":\"coding\",\"love2\":\"movie\"},{\"love1\":\"money\",\"love2\":\"NBA\"}]}";
-//        JSONObject m1=new JSONObject();
-//        m1=JSON.parseObject(jsontext);
+    @RequestMapping(value="/login2", method = RequestMethod.POST)
+    public Object login2(@RequestBody String code) throws Exception{
+        System.out.println(code);
+        String jsontext="{\"name\":\"wjk\",\"age\":\"22\",\"love\":[{\"love1\":\"coding\",\"love2\":\"movie\"},{\"love1\":\"money\",\"love2\":\"NBA\"}]}";
+        JSONObject m1=new JSONObject();
+        m1=JSON.parseObject(jsontext);
+//        return ResultUtil.success(m1);
+        Integer mycode=300;
+        String msg="没有权限";
+        return ResultUtil.error(mycode,msg);
 //        return m1;
-//    }
+    }
 
     @RequestMapping(value="/login", method = RequestMethod.GET)
-    public JSONObject login(@RequestParam(value = "code", required = false) String code){
+    public String login(@RequestParam(value = "code", required = false) String code){
         System.out.println(appId);
         System.out.println(secret);
         System.out.println(code);
-        String url="https://api.weixin.qq.com/sns/jscode2session?appid=" + appId + "&secret=" + secret + "&js_code=" + "001IVJEn1ng6sq0b06Fn1dQtEn1IVJEB" + "&grant_type=" + grantType;
-
+        String url="https://api.weixin.qq.com/sns/jscode2session?appid=" + appId + "&secret=" + secret + "&js_code=" + code + "&grant_type=" + grantType;
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
         String sessionData = responseEntity.getBody();
-        JSONObject json=new JSONObject();
-        json=JSON.parseObject(sessionData);
-//        System.out.println(json.get("openid"));
-//        System.out.println(json.get("session_key"));
-        return json;
+        Login login = JSON.parseObject(sessionData, new TypeReference<Login>() {});
+        System.out.println("111" + login.getOpenid());
+        System.out.println("222" + login.getSession_key());
+        return login.getOpenid();
     }
 }
---
+++ src/main/java/com/zxd/shopping/utils/ResultUtil.java
@@ -0,0 +1,22 @@
+package com.zxd.shopping.utils;
+
+public class ResultUtil {
+    public static Result success(Object object) {
+        Result result = new Result();
+        result.setCode(200);
+        result.setMsg("成功");
+        result.setData(object);
+        return result;
+    }
+
+    public static Result success() {
+        return success(null);
+    }
+
+    public static Result error(Integer code, String msg) {
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        return result;
+    }
+}
